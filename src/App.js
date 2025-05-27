075086da454cc121fadd950112a50039f3be202a
Rajat <rishichoudhary08102002@gmail.com>
2025-05-27 11:01:30 +0530

    Bug Fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import * as THREE from "three";
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { CSS2DRenderer, CSS2DObject } from "three/examples/jsm/renderers/CSS2DRenderer.js";
+import {
+  CSS2DRenderer,
+  CSS2DObject,
+} from "three/examples/jsm/renderers/CSS2DRenderer.js";
 import LabelPlanet from "./Services/LabelPlanet";
 import LockAtPlanet from "./Services/LockAtPlanet";
 import * as dat from "dat.gui";
 import CreatePlanet from "./Services/CreatePlanet";
-import ModalWindow from "./Services/ModalWindow"
+import ModalWindow from "./Services/ModalWindow";
 import starsTexture from "./Images/stars.jpg";
 import sunTexture from "./Images/sun.jpg";
 import mercuryTexture from "./Images/mercury.jpg";
@@ -57,10 +60,10 @@ scene.background = cubeTextureLoader.load([
   starsTexture,
 ]);
 
-const handleViewChange=(e)=>{
+const handleViewChange = (e) => {
   if (e === "Front View") {
     camera.position.set(-90, 140, 140);
-    camera.lookAt(sun)
+    camera.lookAt(sun);
   } else if (e === "Top View") {
     camera.position.set(
       -0.0002439019382374073,
@@ -77,8 +80,8 @@ const handleViewChange=(e)=>{
     camera.lookAt(mercury);
   } else {
   }
-}
-const handleSunGlareChange=(e)=>{
+};
+const handleSunGlareChange = (e) => {
   if (e === true) {
     scene.remove(ambientLight);
     scene.add(pointLight);
@@ -86,27 +89,25 @@ const handleSunGlareChange=(e)=>{
     scene.add(ambientLight);
     scene.remove(pointLight);
   }
-}
-const handleWatchChange=(e)=>{
-  if(e==="---")
-  {
-    options.View="Front View"; 
-    handleViewChange("Front View");
-    options.SunGlare=true;
-    handleSunGlareChange(true);
-    gui.updateDisplay();
-  }
-  else
-  {
-    options.SunGlare=false;
-    handleSunGlareChange(false);
-    gui.updateDisplay();
-  }
-}
+};
+
 
 const gui = new dat.GUI();
 const views = ["---", "Front View", "Top View", "Central View"];
-const watch = ["---","Mercury","Venus","Earth","Mars","Jupiter","Saturn","Jupiter","Saturn","Uranus","Neptune","Pluto"];
+const watch = [
+  "---",
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune",
+  "Pluto",
+];
 const options = {
   Days: 0,
   SunGlare: true,
@@ -126,8 +127,8 @@ setTimeout(() => {
   input.style.pointerEvents = "none";
 }, 0);
 gui.add(options, "SunGlare").onChange(handleSunGlareChange);
-gui.add(options, "Rotation");
-gui.add(options, "Revolution");
+const RotationController=gui.add(options, "Rotation");
+const RevolutionController=gui.add(options, "Revolution");
 const orbits = [
   new THREE.Mesh(
     new THREE.RingGeometry(27, 29, 60),
@@ -167,27 +168,23 @@ const orbits = [
   ),
 ];
 for (let it of orbits) it.rotation.x = -0.5 * Math.PI;
-gui.add(options, "Orbits").onChange((e) => {
+const OrbitsController=gui.add(options, "Orbits").onChange((e) => {
   if (e === true) {
     for (let it of orbits) scene.add(it);
   } else {
     for (let it of orbits) scene.remove(it);
   }
 });
-gui.add(options,"Labels").onChange((e)=>{
-  if(e===true)
-  {
-    for(let {LabelObject,planet} of labelRenderers) planet.add(LabelObject);
+const LabelController=gui.add(options, "Labels").onChange((e) => {
+  if (e === true) {
+    for (let { LabelObject, planet } of labelRenderers) planet.add(LabelObject);
+  } else {
+    for (let { LabelObject, planet } of labelRenderers)
+      planet.remove(LabelObject);
   }
-  else
-  {
-    for(let {LabelObject,planet} of labelRenderers) planet.remove(LabelObject);
-  }
-})
+});
 gui.add(options, "Speed", 0, 10).name("Planet Speed");
-gui.add(options, "MotionSpeed", 0, 100).name("Your Speed");
-gui.add(options, "View", views).onChange(handleViewChange);
-gui.add(options,"Watch",watch).name("Lock At").onChange(handleWatchChange);
+const YourSpeedController=gui.add(options, "MotionSpeed", 0, 100).name("Your Speed");
 
 orbit.addEventListener("change", function (e) {
   if (options.View !== "---") {
@@ -199,87 +196,114 @@ orbit.addEventListener("change", function (e) {
   // }
 });
 
+const handleWatchChange = (e) => {
+  if (e === "---") {
+    options.View = "Front View";
+    handleViewChange("Front View");
+    options.SunGlare = true;
+    handleSunGlareChange(true);
+    gui.updateDisplay();
+    ViewController.domElement.querySelector('select').disabled=false;
+  } else {
+    options.SunGlare = false;
+    handleSunGlareChange(false);
+    gui.updateDisplay();
+    ViewController.domElement.querySelector('select').disabled=true;
+  }
+};
+const ViewController=gui.add(options, "View", views).onChange(handleViewChange);
+gui.add(options, "Watch", watch).name("Lock At").onChange(handleWatchChange);
+
+
 const pointLight = new THREE.PointLight(0xffffff, 10000);
 scene.add(pointLight);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 
 const textureLoader = new THREE.TextureLoader();
 
-
-let labelRenderers=[];
+let labelRenderers = [];
 const sunGeo = new THREE.SphereGeometry(16, 30, 30);
 const sunMat = new THREE.MeshBasicMaterial({
   map: textureLoader.load(sunTexture),
 });
 const sun = new THREE.Mesh(sunGeo, sunMat);
 scene.add(sun);
-labelRenderers.push({...LabelPlanet("Sun",0),planet:sun})
+labelRenderers.push({ ...LabelPlanet("Sun", 0), planet: sun });
 
 const { planet: mercury, planetparent: mercuryparent } = CreatePlanet(
   3.2,
   mercuryTexture,
   28
 );
-labelRenderers.push({...LabelPlanet("Mercury"),planet:mercury});
+labelRenderers.push({ ...LabelPlanet("Mercury"), planet: mercury });
 
 const { planet: venus, planetparent: venusparent } = CreatePlanet(
   5.8,
   venusTexture,
   44
 );
-labelRenderers.push({...LabelPlanet("Venus"),planet:venus});
+labelRenderers.push({ ...LabelPlanet("Venus"), planet: venus });
 
 const { planet: earth, planetparent: earthparent } = CreatePlanet(
   6,
   earthTexture,
   62
 );
-labelRenderers.push({...LabelPlanet("Earth"),planet:earth});
+labelRenderers.push({ ...LabelPlanet("Earth"), planet: earth });
 
 const { planet: mars, planetparent: marsparent } = CreatePlanet(
   4,
   marsTexture,
   78
 );
-labelRenderers.push({...LabelPlanet("Mars"),planet:mars});
+labelRenderers.push({ ...LabelPlanet("Mars"), planet: mars });
 
 const { planet: jupiter, planetparent: jupiterparent } = CreatePlanet(
   12,
   jupiterTexture,
   100
 );
-labelRenderers.push({...LabelPlanet("Jupiter"),planet:jupiter});
+labelRenderers.push({ ...LabelPlanet("Jupiter"), planet: jupiter });
 
 const { planet: saturn, planetparent: saturnparent } = CreatePlanet(
   10,
   saturnTexture,
   138,
-  { innerRadius: 10, outerRadius: 20, texture: saturnRingTexture,tiltangle:0.466 }
+  {
+    innerRadius: 10,
+    outerRadius: 20,
+    texture: saturnRingTexture,
+    tiltangle: 0.466,
+  }
 );
-labelRenderers.push({...LabelPlanet("Saturn"),planet:saturn});
+labelRenderers.push({ ...LabelPlanet("Saturn"), planet: saturn });
 
 const { planet: uranus, planetparent: uranusparent } = CreatePlanet(
   7,
   uranusTexture,
   176,
-  { innerRadius: 7, outerRadius: 12, texture: uranusRingTexture,tiltangle:0.466 }
+  {
+    innerRadius: 7,
+    outerRadius: 12,
+    texture: uranusRingTexture,
+    tiltangle: 0.466,
+  }
 );
-labelRenderers.push({...LabelPlanet("Uranus"),planet:uranus});
+labelRenderers.push({ ...LabelPlanet("Uranus"), planet: uranus });
 
 const { planet: neptune, planetparent: neptuneparent } = CreatePlanet(
   7,
   neptuneTexture,
   200
 );
-labelRenderers.push({...LabelPlanet("Neptune"),planet:neptune});
+labelRenderers.push({ ...LabelPlanet("Neptune"), planet: neptune });
 
 const { planet: pluto, planetparent: plutoparent } = CreatePlanet(
   2.8,
   plutoTexture,
   216
 );
-labelRenderers.push({...LabelPlanet("Pluto"),planet:pluto});
-
+labelRenderers.push({ ...LabelPlanet("Pluto"), planet: pluto });
 
 scene.add(mercuryparent);
 scene.add(venusparent);
@@ -307,11 +331,15 @@ window.addEventListener("keypress", (e) => {
   else if (e.key.toLowerCase() === "d")
     camera.position.add(right.clone().multiplyScalar(-options.MotionSpeed));
 });
-const handleOptionsWatchPlanetChange=(planet,offsetDistance=15)=>{
-  const {cameraPosition,planetWorldPosition}=LockAtPlanet(planet,sun,offsetDistance);
+const handleOptionsWatchPlanetChange = (planet, offsetDistance = 15) => {
+  const { cameraPosition, planetWorldPosition } = LockAtPlanet(
+    planet,
+    sun,
+    offsetDistance
+  );
   camera.position.copy(cameraPosition);
   camera.lookAt(planetWorldPosition);
-}
+};
 function animate() {
   // const overalltime = (performance.now() - starttime) / 1000; //Seconds.MilliSeconds
   // const roundedoffoveralltime=Math.floor(overalltime);
@@ -319,26 +347,42 @@ function animate() {
 
   orbit.update();
 
-  renderer.render(scene,camera);
+  renderer.render(scene, camera);
 
-  if(options.Labels)
-  {
-    for(let {labelRenderer} of labelRenderers) labelRenderer.render(scene,camera)
+  if (options.Labels) {
+    for (let { labelRenderer } of labelRenderers)
+      labelRenderer.render(scene, camera);
   }
 
-  if(options.Watch!=="---")
-  {
-    switch(options.Watch)
-    {
-      case 'Mercury': handleOptionsWatchPlanetChange(mercury); break;
-      case 'Venus': handleOptionsWatchPlanetChange(venus); break;
-      case 'Earth': handleOptionsWatchPlanetChange(earth); break;
-      case 'Mars': handleOptionsWatchPlanetChange(mars); break;
-      case 'Jupiter': handleOptionsWatchPlanetChange(jupiter,20); break;
-      case 'Saturn': handleOptionsWatchPlanetChange(saturn); break;
-      case 'Uranus': handleOptionsWatchPlanetChange(uranus); break;
-      case 'Neptune': handleOptionsWatchPlanetChange(neptune); break;
-      case 'Pluto': handleOptionsWatchPlanetChange(pluto); break;
+  if (options.Watch !== "---") {
+    switch (options.Watch) {
+      case "Mercury":
+        handleOptionsWatchPlanetChange(mercury);
+        break;
+      case "Venus":
+        handleOptionsWatchPlanetChange(venus);
+        break;
+      case "Earth":
+        handleOptionsWatchPlanetChange(earth);
+        break;
+      case "Mars":
+        handleOptionsWatchPlanetChange(mars);
+        break;
+      case "Jupiter":
+        handleOptionsWatchPlanetChange(jupiter, 25);
+        break;
+      case "Saturn":
+        handleOptionsWatchPlanetChange(saturn, 25);
+        break;
+      case "Uranus":
+        handleOptionsWatchPlanetChange(uranus);
+        break;
+      case "Neptune":
+        handleOptionsWatchPlanetChange(neptune);
+        break;
+      case "Pluto":
+        handleOptionsWatchPlanetChange(pluto);
+        break;
     }
   }
 
@@ -353,7 +397,7 @@ function animate() {
   uranus.rotateY(!options.Rotation ? 0 : 0.03);
   neptune.rotateY(!options.Rotation ? 0 : 0.032);
   pluto.rotateY(!options.Rotation ? 0 : 0.008);
-  
+
   mercuryparent.rotateY(!options.Revolution ? 0 : 0.04 * options.Speed);
   venusparent.rotateY(!options.Revolution ? 0 : 0.015 * options.Speed);
   earthparent.rotateY(!options.Revolution ? 0 : 0.01 * options.Speed);
