c512d09a7192b45b48e5ac78bf0e8a477fd8fee7
Rajat <rishichoudhary08102002@gmail.com>
2025-05-28 10:22:36 +0530

    Bug fixed for timer in modal window

--- src/Services/ModalWindow.js
+++ src/Services/ModalWindow.js
@@ -1,18 +1,23 @@
 import "./ModalWindow.css";
-export default function ModalWindow(textprop,timerprop=10) {
+export default function ModalWindow(textprop, timerprop = 10) {
   setTimeout(() => {
     const modal = document.getElementById("instruction-modal");
     const closeButton = document.getElementById("close-modal");
     const timer = document.querySelector(".timer");
-    const text=document.querySelector("#instruction-modal p");
+    const text = document.querySelector("#instruction-modal p");
     modal.style.display = "flex";
-    text.innerHTML=textprop;
-    timer.innerHTML=timerprop;
+    text.innerHTML = textprop;
+    timer.innerHTML = timerprop;
     closeButton.addEventListener("click", () => {
       modal.style.display = "none";
     });
-    const bodyClickHandler = () => {
+    const clearEverything = () => {
       modal.style.display = "none";
+      document.body.removeEventListener("click", bodyClickHandler);
+      clearInterval(id);
+    };
+    const bodyClickHandler = () => {
+      clearEverything();
     };
     const timerHandler = () => {
       timer.innerHTML = String(Number(timer.innerHTML) - 1);
@@ -21,9 +26,7 @@ export default function ModalWindow(textprop,timerprop=10) {
     document.body.addEventListener("click", bodyClickHandler);
 
     setTimeout(() => {
-      modal.style.display = "none";
-      document.body.removeEventListener("click", bodyClickHandler);
-      clearInterval(id);
-    }, timerprop*1000);
+      clearEverything();
+    }, timerprop * 1000);
   }, 0);
 }
